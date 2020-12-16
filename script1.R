1c0db9e19c7017e6d6f2fd4205c0f1f049eb0db7
Tarik Sijercic <tsijercic1@etf.unsa.ba>
2020-12-16 19:54:48 +0100

    Basic data read-write

--- script1.R
+++ script1.R
@@ -4,3 +4,26 @@ require("LiblineaR") # library("LiblineaR") can be used instead
 ? LiblineaR # show info on package
 detach("package:LiblineaR", unload=TRUE)
 remove.packages("LiblineaR")
+
+str(iris) # sample data
+
+myRange <- 1:10
+myVector <- c(1,2,3,4,5)
+mySequence <- seq(1,20, by=2)
+arrayInput <- scan() # terminates the input on empty field + enter
+
+rm(myRange)
+remove(myVector)
+remove(mySequence)
+remove(arrayInput)
+
+# Rstudio isn't inserting tabs as tabs (in my case that is)...
+employees <- read.table("./data/Employees.txt", header = TRUE, sep = "\t")
+str(employees)
+
+employees2 <-read.csv("./data/Employees.csv", header = TRUE)
+str(employees2)
+
+View(employees)
+
+remove(list = ls()) # ls() list all variables in the environment
